537c82ff41d9faa1d4bcf800ae4802fae9e79edb
AsherRohde <c.asher.rohde@gmail.com>
2017-12-22 13:35:33 -0700

    Initial Commit
    Early version of the game with character and randomly generated blocks

---
+++ Mining Game/Assets/Scripts/ClickMover.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+private Animator anim;
+public float speed = 1.5f;
+private Vector3 target;
+private Vector3 MousePosition;
+private Vector3 PlayerPosition;
+void Start()
+{
+    anim = GetComponent<Animator>();
+    target = transform.position;
+}
+void Update()
+{
+    if (Input.GetMouseButtonDown(0))
+    {
+        target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        target.z = transform.position.x;
+        Debug.Log("traaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" + target.z);
+        if (target.x <= target.z)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            anim.SetInteger("Direction", 1);
+        }
+        else if (target.x >= target.z)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+            anim.SetInteger("Direction", 1);
+        }
+    }
+    transform.position = Vector3.MoveTowards(transform.position, target, speedTime.deltaTime);
+}
+     }
